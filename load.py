c8b7c8192ba96361759a509f3a849b6f3891e8f1
0xkf <Kanichi73@me.com>
2023-04-08 22:42:52 +0900

    recent

--- load.py
+++ load.py
@@ -6,6 +6,8 @@ load_dotenv()
 MYAPI = os.getenv('MYAPI')
 # THEURL = os.getenv('THEURL')
 QUESTION1 = os.getenv('QUESTION1')
+QUESTION2 = os.getenv('QUESTION2')
+QUESTION3 = os.getenv('QUESTION3')
 
 os.environ["OPENAI_API_KEY"] = MYAPI
 json_path = "./data/wiki.json"
@@ -15,3 +17,5 @@ index = GPTSimpleVectorIndex.load_from_disk(json_path)
 
 # print(index.query("聖書の内容を要約してください。？"))
 print(index.query(QUESTION1))
+print(index.query(QUESTION2))
+print(index.query(QUESTION3))
